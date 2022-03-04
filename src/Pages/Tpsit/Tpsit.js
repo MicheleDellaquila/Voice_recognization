33f1a1d9e8c5769ed142986dce610ab75aac2a9e
Michele Dellaquila <micheledellaquila2003@gmail.com>
2022-03-04 10:47:19 +0100

    complete page tpsit

--- src/Pages/Tpsit/Tpsit.js
+++ src/Pages/Tpsit/Tpsit.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './Tpsit.scss';
 import Cover from '../../Reusable/Cover/Cover';
 import Header from '../../Containers/Header/Header';
+import Tecnology from '../../Containers/Tecnology/Tecnology';
+import Footer from '../../Containers/Footer/Footer';
 
 const Tpsit = () => {
   return (
@@ -13,6 +16,10 @@ const Tpsit = () => {
           backgroundColor='#00203F'
         />
       </div>
+      <div className='Tpsit__tecnology container'>
+        <Tecnology />
+      </div>
+      <Footer />
     </div>
   );
 };
