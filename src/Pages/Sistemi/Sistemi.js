41556138bc3ec272ddce0200ab172ecce30eeba1
Michele Dellaquila <micheledellaquila2003@gmail.com>
2022-03-05 09:49:36 +0100

    minor fix

--- src/Pages/Sistemi/Sistemi.js
+++ src/Pages/Sistemi/Sistemi.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './Sistemi.scss';
 import Cover from '../../Reusable/Cover/Cover';
 import Header from '../../Containers/Header/Header';
+import Footer from '../../Containers/Footer/Footer';
+import Web from '../../Containers/Web2.0/Web2.0';
 
 const Sistemi = () => {
   return (
@@ -9,10 +12,14 @@ const Sistemi = () => {
         <Header />
         <Cover
           title='Sistemi'
-          subtitle='Web 2.0, cose e quali sono i suoi utilizzi.'
+          subtitle="Web 2.0, cos'è e quali sono i suoi utilizzi."
           backgroundColor='#CC313D'
         />
       </div>
+      <div className='Sistemi__tecnology container'>
+        <Web />
+      </div>
+      <Footer />
     </div>
   );
 };
