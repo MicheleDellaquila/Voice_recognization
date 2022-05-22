3d213dcc5cafb3cf4da78079743baf58353e0b38
Michele Dellaquila <micheledellaquila2003@gmail.com>
2022-05-22 08:51:02 +0200

    minor fix

--- src/Containers/heroGestione/heroGestione.js
+++ src/Containers/heroGestione/heroGestione.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './heroGestione.scss';
-import educazioneCivicaPage from '../../assets/images/educazioneCivicaPage.jpg';
+import GestionePage from '../../assets/images/gestionePage.png';
 
 const HeroGestione = () => {
   return (
     <div className='HeroGestione'>
       <div className='HeroGestione__container'>
         <div className='HeroGestione__row'>
-          <div className='col-xs-12 col-md-10 col-lg-5'>
+          <div className='col-xs-12 col-md-10 col-lg-7'>
             <h1 className='HeroGestione__title'>
               Ambience App - Spiegazione di un ciclo di vita di un progetto.
             </h1>
@@ -16,8 +16,8 @@ const HeroGestione = () => {
               ambientale, controllando l'ambiente in cui viviamo
             </p>
           </div>
-          <div className='col-xs-12 col-md-10 col-lg-7'>
-            <img src={educazioneCivicaPage} alt='gestione' />
+          <div className='col-xs-12 col-md-10 col-lg-4'>
+            <img src={GestionePage} alt='gestione' />
           </div>
         </div>
       </div>
